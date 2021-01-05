6f4cba67eb36a0f3eab20e4e8d79cf23f977c80c
sabljakovich <hamza.sabljakovic@edu.fit.ba>
2021-01-05 18:49:38 +0100

    Add mysql replication health check

---
+++ MySQLReplicationHealthCheck/MySQLReplicationHealthCheck.cs
@@ -0,0 +1,48 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Dapper;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using MySql.Data.MySqlClient;
+
+namespace MySQLReplicationHealthCheck
+{
+    public class MySQLReplicationHealthCheck : IHealthCheck
+    {
+        private readonly string _mySQLConnectionString;
+
+        private readonly int SLAVE_IO_RUNNING_INDEX = 10;
+        private readonly int SLAVE_SQL_RUNNING_INDEX = 11;
+
+        public MySQLReplicationHealthCheck(string mySQLConnectionString)
+        {
+            _mySQLConnectionString = mySQLConnectionString;
+        }
+
+        public Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+        {
+
+            using (var connection = new MySqlConnection(_mySQLConnectionString))
+            {
+
+                var slaves = connection.ExecuteReader("SHOW SLAVE STATUS;");
+
+                if (slaves == null)
+                {
+                    return Task.FromResult(HealthCheckResult.Unhealthy("Replication not working"));
+                }
+
+                while (slaves.Read())
+                {
+                    if (slaves.GetString(SLAVE_IO_RUNNING_INDEX) != "YES" || slaves.GetString(SLAVE_SQL_RUNNING_INDEX) != "YES")
+                    {
+                        Task.FromResult(HealthCheckResult.Unhealthy("Replication not working"));
+                    }
+                }
+
+            }
+
+            return Task.FromResult(HealthCheckResult.Healthy("Replication OK"));
+        }
+    }
+
+}
---
+++ MySQLReplicationHealthCheck/MySQLReplicationHealthCheckBuilderExtension.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace MySQLReplicationHealthCheck
+{
+    public static class MySQLReplicationHealthCheckBuilderExtension
+    {
+        public static IHealthChecksBuilder AddMySqlReplication(this IHealthChecksBuilder builder, string connectionString, string name = default, HealthStatus? failureStatus = default, IEnumerable<string> tags = default, TimeSpan? timeout = default)
+        {
+            return builder.Add(new HealthCheckRegistration(
+                name ?? "MySQL Replication",
+                serviceProvider => new MySQLReplicationHealthCheck(connectionString),
+                failureStatus,
+                tags,
+                timeout
+            ));
+        }
+    }
+}
